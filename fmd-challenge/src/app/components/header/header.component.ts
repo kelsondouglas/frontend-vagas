dad5469ab4523ea37462124791a3f6fbaa3ad184
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-09-26 20:30:25 -0300

    feat(header): add mobile menu

--- fmd-challenge/src/app/components/header/header.component.ts
+++ fmd-challenge/src/app/components/header/header.component.ts
@@ -1,14 +1,24 @@
-import { NgOptimizedImage } from '@angular/common';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { NAVBAR_LINKS } from '../../app.constants';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   navbarLinks = NAVBAR_LINKS;
+
+  isMenuOpen = false;
+
+  closeMobileMenu() {
+    this.isMenuOpen = false;
+  }
+
+  toggleMobileMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
