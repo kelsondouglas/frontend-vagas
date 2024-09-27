55f08e26d7c0dd0f507bcb155268dc2b002c04e9
Kelson Douglas <kelsondouglas23@hotmail.com>
2024-09-27 16:12:48 -0300

    feat(rating): add carousel and responsiviness to rating cards

--- fmd-challenge/src/app/components/rating-section/rating-section.component.ts
+++ fmd-challenge/src/app/components/rating-section/rating-section.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { RatingCardComponent } from '../rating-card/rating-card.component';
+import { CarouselModule } from 'primeng/carousel';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-rating-section',
   standalone: true,
-  imports: [RatingCardComponent],
+  imports: [RatingCardComponent, CarouselModule, NgOptimizedImage],
   templateUrl: './rating-section.component.html',
   styleUrl: './rating-section.component.scss',
 })
-export class RatingSectionComponent {}
+export class RatingSectionComponent {
+  ratings = Array.from({ length: 3 }, (_, i) => i + 1);
+  activeIndex = 0;
+
+  onChange(event: any) {
+    this.activeIndex = event.page;
+  }
+}
